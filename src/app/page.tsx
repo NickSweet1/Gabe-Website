eb15c46f9503204ae6c01c656da6a7c0df0a0803
Nick <nicksweetj@gmail.com>
2024-01-05 00:01:51 -0500

    temporary background image is on hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,34 @@
 "use client";
 import NavBar from "./components/navbar";
 import { HeartIcon } from "@heroicons/react/24/outline";
+import heroBackgroundImage from "../../public/gabe_building.jpg";
+import Image from "next/image";
+import AboutPage from "./about/page";
+
 export default function Home() {
   return (
-    <div>
+    <div className="relative bg-cover bg-center">
+      <Image
+        src={heroBackgroundImage}
+        alt="Background Image"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        priority={true}
+        />
       <NavBar />
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
+          >
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl flex items-center justify-center">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl flex items-center justify-center">
               Hey there, friend! <HeartIcon className="h-10 w-10 ml-2" />
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-lg leading-8 text-white">
               The name's Gabe, and making buildings is my game. Let's make some
               big towers!
             </p>
@@ -31,13 +36,13 @@ export default function Home() {
               <a
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
+                >
                 Get started
               </a>
               <a
                 href="#"
                 className="text-sm font-semibold leading-6 text-gray-900"
-              >
+                >
                 Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
@@ -46,14 +51,7 @@ export default function Home() {
         <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
+          >
         </div>
       </div>
     </div>
