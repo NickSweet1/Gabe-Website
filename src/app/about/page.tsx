821b32980ce83dc36769440b069903d1d34ebec6
Nick <nicksweetj@gmail.com>
2024-01-05 00:28:19 -0500

    started the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import goober from '../../../public/goober-gabe.jpg';
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
+    <div>
+      <h2>About Gabe</h2>
+      <p>Meet Gabe, the friendly goober!</p>
+      <p>Let me make your buildings big and strong!</p>
+      <Image src={goober} alt="Gabe the goober" />
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
